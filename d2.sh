ef511fa94c4d71bcd8f6f414f16e9ae555ce300d
Hxlxnn <105493840+Hxlxnn@users.noreply.github.com>
2024-01-13 02:57:05 +0100

    Update d2.sh

--- d2.sh
+++ d2.sh
@@ -1,32 +1,39 @@
 #!/bin/bash
 
-# rajouter pour mettre dans l'ordre décroissant
-filePath="data.csv"
+# Chemin vers le fichier de données
+cheminFichier="data.csv"
 
-if [[ ! -f "$filePath" ]]; then
-    echo "Le fichier $filePath n'existe pas. Veuillez vérifier le chemin du fichier."
+if [[ ! -f "$cheminFichier" ]]; then
+    echo "Le fichier $cheminFichier n'existe pas. Veuillez vérifier le chemin du fichier."
     exit 1
 fi
 
-# Reste du script inchangé...
+# Suite du script inchangée...
+temps_debut=$(date +%s.%N)
 
- echo "d2 arg found"
-        # need to sort by driver name
-        # then add each distance
-        # output the top 10 ones with distances to make the graph
-        # make the graph
+echo "d2 arg trouvé"
+# Besoin de trier par le nom du conducteur
+# Puis ajouter chaque distance
+# Afficher les 10 premiers avec les distances pour créer le graphique
 
-        # separate in fields with ;, create a array of sum[route ID] += distance, then print each route ID with it's sum (with 3 decimals)
-        echo "Summing drivers routes..."
-       LC_NUMERIC=C awk -F';' 'NR>1{sum[$6]+=$5} END{for(i in sum) printf "%s;%.6f\n", i, sum[i]}' ${filePath} >"./temp/d2_argument_sum.csv"
-        # sort the value from the second field (length), and only numerical, and reversed to have the longest on top
-        echo "Sorting drivers routes..."
-        sort -t';' -k2nr "./temp/d2_argument_sum.csv" >"./temp/sorted_d2_argument_sum.csv"
-        echo "Sorting drivers..."
-        # get the top 10 longest route
-        head -n 10 "./temp/sorted_d2_argument_sum.csv" >"./temp/d2_argument_top10.csv"
-         cat "./temp/d2_argument_top10.csv" # to show the top 10
+# Séparer les champs avec ;, créer un tableau sum[route ID] += distance, puis imprimer chaque route ID avec sa somme (avec 3 décimales)
+echo "Somme des distances par conducteur..."
+LC_NUMERIC=C awk -F';' 'NR>1{sum[$6]+=$5} END{for(i in sum) printf "%s;%.6f\n", i, sum[i]}' ${cheminFichier} >"./temp/d2_argument_sum.csv"
 
-        
+# Trier les valeurs selon le deuxième champ (longueur), uniquement numérique, et en ordre inversé pour avoir les plus longues au sommet
+echo "Tri des trajets par conducteur..."
+sort -t';' -k2nr "./temp/d2_argument_sum.csv" >"./temp/sorted_d2_argument_sum.csv"
 
+echo "Tri des conducteurs..."
+# Obtenir les 10 premiers trajets les plus longs
+head -n 10 "./temp/sorted_d2_argument_sum.csv" >"./temp/d2_argument_top10.csv"
+cat "./temp/d2_argument_top10.csv" # Affichage des 10 premiers
 
+#Fin chronomètre
+temps_fin=$(date +%s.%N)
+
+# Calculez la durée de traitement en secondes
+duree_traitement=$(echo "($temps_fin - $temps_debut)/1" | bc)
+
+# Affichez la durée de traitement
+echo "Le traitement d2 a pris $duree_traitement secondes pour s'exécuter."
