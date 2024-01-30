b2f17d0c095a02fa327f6f4753ab347c7fba960e
Hxlxnn <105493840+Hxlxnn@users.noreply.github.com>
2024-01-30 19:40:28 +0100

    Update t.sh

--- progc/t.sh
+++ progc/t.sh
@@ -47,9 +47,23 @@ END {
 #compilation du programme C
 gcc -o t t.c
 
-#exécution du programme C
-./t > "$fichier_final"
-#Fin chronomètre
+# Vérifier si la compilation a réussi
+if [ $? -eq 0 ]; then
+    echo "Compilation réussie. Exécution du programme..."
+
+    # Exécuter le programme C + insertion des dponnées de sortie dans le fichier résultat
+    ./t > "$fichier_final"
+
+    # Vérifier si l'exécution s'est terminée avec succès
+    if [ $? -eq 0 ]; then
+        echo "Exécution réussie. Création du graphe." //création du graphe à rajouter
+    else
+        echo "Erreur lors de l'exécution du programme."
+    fi
+
+else
+    echo "Erreur lors de la compilation du programme C."
+fi
 temps_fin=$(date +%s.%N)
 
 # Calculez la durée de traitement en secondes
